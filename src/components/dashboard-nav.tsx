a6d05cbed1ff173b4683c5fbcd604d1c67af91c2
issac <63854143+jadstrike@users.noreply.github.com>
2024-04-20 16:40:30 +0700

    finished logout

--- src/components/dashboard-nav.tsx
+++ src/components/dashboard-nav.tsx
@@ -2,11 +2,13 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import { Icons } from "@/components/icons";
 import { cn } from "@/lib/utils";
 import { NavItem } from "@/types";
 import { Dispatch, SetStateAction } from "react";
+import { toast } from "sonner";
 
 interface DashboardNavProps {
   items: NavItem[];
@@ -15,29 +17,66 @@ interface DashboardNavProps {
 
 export function DashboardNav({ items, setOpen }: DashboardNavProps) {
   const path = usePathname();
+  const router = useRouter();
 
   if (!items?.length) {
     return null;
   }
 
+  const logout = async () => {
+    const res = await fetch("/api/logout", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (res.ok) {
+      toast("User successfully logged Out", {
+        action: {
+          label: "ok",
+          onClick: () => console.log("ok"),
+        },
+      });
+      router.push("/");
+    }
+  };
+
   return (
     <nav className="grid items-start gap-2">
       {items.map((item, index) => {
         const Icon = Icons[item.icon || "arrowRight"];
-        return (
+        return item.href && item.button ? (
+          <button
+            key={index}
+            onClick={() => {
+              if (setOpen) setOpen(false);
+              item.button ? logout() : null;
+            }}
+            className={cn(
+              "group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
+              path === item.href ? "bg-accent" : "transparent",
+              item.disabled && "cursor-not-allowed opacity-80"
+            )}
+          >
+            <Icon className="mr-2 h-4 w-4" />
+            <span>{item.title}</span>
+          </button>
+        ) : (
           item.href && (
             <Link
               key={index}
               href={item.disabled ? "/" : item.href}
               onClick={() => {
                 if (setOpen) setOpen(false);
+                item.button ? logout() : null;
               }}
             >
               <span
                 className={cn(
                   "group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
                   path === item.href ? "bg-accent" : "transparent",
-                  item.disabled && "cursor-not-allowed opacity-80",
+                  item.disabled && "cursor-not-allowed opacity-80"
                 )}
               >
                 <Icon className="mr-2 h-4 w-4" />

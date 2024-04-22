e6b93604447dd9ef3d36903b0746bc91b3f6643f
LazyFox <heinzayarc@gmail.com>
2024-04-23 01:55:11 +0700

    Finished Ban and Unban User Functions

--- src/app/api/users/[user_id]/ban/route.ts
+++ src/app/api/users/[user_id]/ban/route.ts
@@ -5,19 +5,37 @@ const prisma = new PrismaClient();
 
 export async function PUT(request: NextRequest) {
   const userId = request.url.split("/")[5];
-  const user = await prisma.user.findUnique({ where: { id: userId } });
 
-  if (user?.isBanned) {
-    return NextResponse.json({ message: "This user is already banned." });
-  } else {
-    const bannedUser = await prisma.user.update({
-      where: { id: user?.id },
-      data: { isBanned: true },
-    });
+  try {
+    const user = await prisma.user.findUnique({ where: { id: userId } });
 
-    return NextResponse.json({
-      message: "Successfully banned the user",
-      bannedUser,
-    });
+    if (user?.isBanned) {
+      return NextResponse.json({ message: "This user is already banned." });
+    } else {
+      const bannedUser = await prisma.user.update({
+        where: { id: user?.id },
+        data: { isBanned: true },
+      });
+
+      const hiddenIdeasByUser = await prisma.idea.updateMany({
+        where: { authorId: user?.id },
+        data: { isHidden: true },
+      });
+
+      const hiddenCommentsByUser = await prisma.comment.updateMany({
+        where: { userId: user?.id },
+        data: { isHidden: true },
+      });
+
+      return NextResponse.json({
+        message: "Successfully banned the user",
+        bannedUser,
+        hiddenIdeasByUser,
+        hiddenCommentsByUser,
+      });
+    }
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
--- src/app/api/users/[user_id]/unban/route.ts
+++ src/app/api/users/[user_id]/unban/route.ts
@@ -5,19 +5,39 @@ const prisma = new PrismaClient();
 
 export async function PUT(request: NextRequest) {
   const userId = request.url.split("/")[5];
-  const user = await prisma.user.findUnique({ where: { id: userId } });
 
-  if (!user?.isBanned) {
-    return NextResponse.json({ message: "This user is not in banned state." });
-  } else {
-    const bannedUser = await prisma.user.update({
-      where: { id: user?.id },
-      data: { isBanned: false },
-    });
+  try {
+    const user = await prisma.user.findUnique({ where: { id: userId } });
 
-    return NextResponse.json({
-      message: "Successfully unbanned the user",
-      bannedUser,
-    });
+    if (!user?.isBanned) {
+      return NextResponse.json({
+        message: "This user is not in banned state.",
+      });
+    } else {
+      const unbannedUser = await prisma.user.update({
+        where: { id: user?.id },
+        data: { isBanned: false },
+      });
+
+      const shownIdeasByUser = await prisma.idea.updateMany({
+        where: { authorId: user?.id },
+        data: { isHidden: false },
+      });
+
+      const shownCommentsByUser = await prisma.comment.updateMany({
+        where: { userId: user?.id },
+        data: { isHidden: false },
+      });
+
+      return NextResponse.json({
+        message: "Successfully unbanned the user",
+        unbannedUser,
+        shownIdeasByUser,
+        shownCommentsByUser,
+      });
+    }
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }

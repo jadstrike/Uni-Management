7807fec7ad23c86aab951269da03a99f368bde5a
issac <63854143+jadstrike@users.noreply.github.com>
2024-04-22 22:27:27 +0700

    making idea complete

--- src/app/(dashboard)/dashboard/idea/add/page.tsx
+++ src/app/(dashboard)/dashboard/idea/add/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { Label } from "@/components/ui/label";
+import { toast } from "sonner";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useEffect } from "react";
@@ -32,10 +35,22 @@ const breadcrumbItems = [
   { title: "Add new idea", link: "/dashboard/catrgories/add" },
 ];
 const formSchema = z.object({
-  title: z.string().min(2).max(50),
-  content: z.string().min(2),
-  file: z.string(),
-  category: z.string(),
+  title: z.string().min(2).max(30, {
+    message: "Title must be at least 2 characters.",
+  }),
+  content: z.string().min(2, {
+    message: "content must be at least 2 characters.",
+  }),
+  file:
+    typeof FileList !== "undefined"
+      ? z.instanceof(FileList).optional()
+      : z.unknown().optional(),
+
+  category: z
+    .string({
+      required_error: "Please select an email to display.",
+    })
+    .default(""),
 });
 
 interface Category {
@@ -44,18 +59,17 @@ interface Category {
 }
 
 export default function AddIdea() {
+  const supabase = createClientComponentClient();
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(
+    null
+  );
   const [categories, setCategories] = useState<Category[]>([]);
   const [loading, setLoading] = useState(true);
-  console.log(categories);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-      content: "",
-      file: "",
-      category: "",
-    },
   });
+
+  const fileRef = form.register("file");
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -69,58 +83,173 @@ export default function AddIdea() {
     fetchCategories();
   }, []);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await axios.get("/api/categories");
-  //     console.log(response.data.categories);
-  //   };
-  //   fetchData();
-  // }, []);
+  //FOR FILE UPLOAD
+  // Handle file upload event
+  const uploadFile = async (userfile: any) => {
+    console.log(userfile);
+    const file = userfile[0];
+
+    const bucket = "files";
+
+    // Call Storage API to upload file
+    const { data, error } = await supabase.storage
+      .from(bucket)
+      .upload(file.name, file);
+
+    // Handle error if upload failed
+    if (error) {
+      alert("Error uploading file.");
+      return;
+    }
+
+    const document = supabase.storage.from("files").getPublicUrl(data.path);
+    const url = document.data.publicUrl;
+    // console.log(url);
+    toast.success("File uploaded successfully!", {
+      duration: 5000,
+    });
+    return url;
+
+    // await fetch("http://localhost:3000/api/documents", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: url,
+    // });
+
+    // alert("File uploaded successfully!");
+  };
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    const file = values.file;
+    const fileData = await uploadFile(file);
+    console.log("File Data" + fileData);
+
+    try {
+      const response = await axios.post(`http://localhost:3000/api/ideas`, {
+        title: values.title,
+        content: values.content,
+        category: values.category,
+        file: fileData,
+
+        // add more data if needed
+      });
+      console.log(response.data);
+      // alert(response.data.message);
+      toast.success(response.data.message, {
+        duration: 5000,
+      });
+      // <Toaster richColors />;
+      form.reset();
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred. Please try again.");
+    }
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
 
   return (
-    <div className="flex-1 space-y-4 p-5">
-      <BreadCrumb items={breadcrumbItems} />
-      <div className="w-full px-4 py-8 sm:px-6 md:max-w-md md:mx-auto md:py-12">
-        <h1 className="text-3xl font-bold mb-6">Add New Idea</h1>
-        <Form {...form}>
-          <form className="space-y-4">
-            <div>
-              <Label htmlFor="title">Title</Label>
-              <Input id="title" placeholder="Enter a title for your idea" />
-            </div>
-            <div>
-              <Label htmlFor="content">Content</Label>
-              <Textarea
-                className="min-h-[120px]"
-                id="content"
-                placeholder="Describe your idea"
+    <ScrollArea className="h-full">
+      <div className="flex-1 space-y-4 p-5">
+        <BreadCrumb items={breadcrumbItems} />
+        <div className="w-full px-4 py-8 sm:px-6 md:max-w-md md:mx-auto md:py-12">
+          <h1 className="text-3xl font-bold mb-6">Add New Idea</h1>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter a title for your idea"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-            </div>
-            <div>
-              <Label htmlFor="file">Attachment</Label>
-              <Input id="file" type="file" />
-            </div>
-            <div>
-              <Label htmlFor="category">Category</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select a category" />
-                </SelectTrigger>
-                <SelectContent>
-                  {categories.map((category, index) => (
-                    <SelectItem key={category.id} value={category.name}>
-                      {category.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-            <Button className="w-full" type="submit">
-              Submit Idea
-            </Button>
-          </form>
-        </Form>
+              <FormField
+                control={form.control}
+                name="content"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Content</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        {...field}
+                        className="min-h-[120px]"
+                        placeholder="Describe your idea"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="file"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Attachment</FormLabel>
+                    <FormControl>
+                      <Input
+                        onChange={(event) => {
+                          field.onChange(event.target?.files?.[0] ?? undefined);
+                        }}
+                        {...fileRef}
+                        type="file"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="category"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Category</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a category" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {categories.map((category, index) => (
+                          <SelectItem key={category.id} value={category.name}>
+                            {category.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+
+              <Button className="w-full" type="submit">
+                Submit Idea
+              </Button>
+            </form>
+          </Form>
+        </div>
       </div>
-    </div>
+    </ScrollArea>
   );
 }
